8192240002a856a1308bfd468e99385f562bcd8d
Pradipa Y <pdpyyo@gmail.com>
2026-01-02 15:02:05 +0700

    menambahkan interface dan class utnuk ContainerApp.kt

--- app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -1,2 +1,14 @@
 package com.example.firebase.repositori
 
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp override fun onCreate() {
+        super.onCreate() this.container = DefaultContainerApp() }
+}
